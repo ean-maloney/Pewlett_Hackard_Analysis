2257c8c0a869c03b19c1a0fad7c0324594ed1cf3
Ean Maloney <eanmaloney@gmail.com>
2021-06-02 09:43:04 -0400

    Adding queries and stuff.

--- schema.sql
+++ schema.sql
@@ -36,13 +36,12 @@ CREATE TABLE salaries(
 );
 
 CREATE TABLE dept_emp(
-	dept_no VARCHAR(4) NOT NULL,
 	emp_no INT NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 	FOREIGN KEY(dept_no) REFERENCES departments(dept_no),
-	FOREIGN KEY(emp_no) REFERENCES employees(emp_no),
-	PRIMARY KEY(emp_no)
+	FOREIGN KEY(emp_no) REFERENCES employees(emp_no)
 );
 
 CREATE TABLE titles(
@@ -50,8 +49,8 @@ CREATE TABLE titles(
 	title VARCHAR NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
-	FOREIGN KEY(emp_no) REFERENCES employees(emp_no),
-	PRIMARY KEY(emp_no)
+	FOREIGN KEY(emp_no) REFERENCES employees(emp_no)
 );
 
-SELECT * FROM departments;
+DROP TABLE titles CASCADE
+SELECT * FROM titles
